44700c94ae5213684937585dc1d880720954cebf
Heitor Carvalho Ferreira <167567826+Ferre1ra10@users.noreply.github.com>
2026-05-15 22:24:32 -0300

    Delete login-completo directory

--- login-completo/middlewares/sanitizarDados.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res, next) {
-    req.body.nome = req.body.nome.trim();
-    req.body.email = req.body.email.trim().toLowerCase();
-    req.body.cpf = req.body.cpf.trim();
-
-    next();
-};
